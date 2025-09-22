a2629d95380338a3ff473b78a7a306fafcfd8447
GeorgeTheScripter <albegoff.georgy@yandex.ru>
2025-09-22 21:38:03 +0300

    старт проекта

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import Profile from './components/Profile.vue'
+</script>
+
+<template>
+  <div class="app">
+    <div class="menu">
+      <Profile name="Боб" />
+    </div>
+    <div class="content">content</div>
+  </div>
+</template>
+
+<style scoped>
+.app {
+  display: flex;
+  gap: 200px;
+  min-height: calc(100vh-140px);
+  max-width: 1400px;
+  margin: 140px auto 0;
+}
+</style>
---
+++ src/components/Profile.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const { name } = defineProps<{
+  name: string
+}>()
+</script>
+
+<template>
+  <div class="menu-head">
+    <img class="menu-head__img" src="/src/assets/avatar.png" alt="avatar" />
+    <div class="menu-head__greetings">
+      Привет, <span>{{ name }}</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.menu-head {
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+}
+.menu-head__greetings {
+  font-size: 18px;
+  line-height: 100%;
+}
+
+.menu-head__greetings span {
+  font-weight: 700;
+}
+</style>
