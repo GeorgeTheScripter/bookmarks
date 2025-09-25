9f04e7b523423e0b23def2af11af90128af8db38
GeorgeTheScripter <albegoff.georgy@yandex.ru>
2025-09-25 12:27:31 +0300

    Добавлен статус пользователя

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,24 +1,39 @@
 <script setup lang="ts">
 import { useProfileStore } from '@/stores/profile.store';
-import { onMounted } from 'vue';
+import { computed, onMounted } from 'vue';
+import Status from './Status.vue';
 
 const store = useProfileStore();
 
 onMounted(() => {
   store.fetchProfile();
 });
+
+const userName = computed(() => {
+  return store.profile ? store.profile.name : 'Пользователь';
+});
 </script>
 
 <template>
   <div class="menu-head">
-    <img class="menu-head__img" src="/src/assets/avatar.png" alt="avatar" />
+    <div class="avatar__info">
+      <img class="menu-head__img" src="/src/assets/avatar.png" alt="avatar" />
+
+      <Status />
+    </div>
+
     <div class="menu-head__greetings">
-      Привет, <span>{{ store.profile?.name }}</span>
+      Привет, <span>{{ userName }}</span>
     </div>
   </div>
 </template>
 
 <style scoped>
+.avatar__info {
+  position: relative;
+  width: fit-content;
+}
+
 .menu-head {
   display: flex;
   flex-direction: column;
---
+++ src/components/Status.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useProfileStore } from '@/stores/profile.store';
+
+const store = useProfileStore();
+
+const statusColor = computed(() => {
+  return store.profile ? 'green' : 'red';
+});
+</script>
+
+<template>
+  <div class="status" :style="{ background: statusColor }"></div>
+</template>
+
+<style scoped>
+.status {
+  width: 12px;
+  height: 12px;
+  border-radius: 12px;
+  border: 2px solid var(--color-bg);
+  position: absolute;
+  bottom: 10px;
+  right: 4px;
+}
+</style>
